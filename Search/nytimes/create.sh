d16cf4c20023f2ff30b7eaa048973986ddbeb37d
shkediy <yuval.shkedi@gmail.com>
2019-05-02 16:18:05 -0400

    added readme

--- Search/nytimes/create.sh
+++ Search/nytimes/create.sh
@@ -10,8 +10,8 @@ if [ "X$HOST" == "X" -o "X$PORT" == "X" -o  "X$INDEX" == "X" ]; then
 fi
 
 echo "Creating index..."
-python2 NYTimes.py -s $HOST -p $PORT -i $INDEX -c
+python NYTimes.py -s $HOST -p $PORT -i $INDEX -c
 
 echo "Loading data..."
-python2 ../ImportCSV.py -s $HOST -p $PORT -i $INDEX -r true  -c 2 -g 1 14 -t 11 -o "%Y-%m-%d %H:%M:%S"  -f Articles.csv 
+python ../ImportCSV.py -s $HOST -p $PORT -i $INDEX -r true  -c 2 -g 1 14 -t 11 -o "%Y-%m-%d %H:%M:%S"  -f Articles.csv 
 echo "Done."
--- Search/sales/create.sh
+++ Search/sales/create.sh
@@ -10,8 +10,8 @@ if [ "X$HOST" == "X" -o "X$PORT" == "X" -o  "X$INDEX" == "X" ]; then
 fi
 
 echo "Creating index..."
-python2 SalesDemo.py -s $HOST -p $PORT -i $INDEX -c
+python SalesDemo.py -s $HOST -p $PORT -i $INDEX -c
 
 echo "Loading data..."
-python2 ../ImportCSV.py -s $HOST -p $PORT -i $INDEX -r true  -f sales_data_sample.csv
+python ../ImportCSV.py -s $HOST -p $PORT -i $INDEX -r true  -f sales_data_sample.csv
 echo "Done."
